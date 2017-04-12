006285ead8138b2ed13f5dab921439b74fc704c0
Bevard <sbevard1@gmail.com>
2017-04-12 17:18:04 -0400

    small changes

--- Main.js
+++ Main.js
@@ -1,9 +1,17 @@
+//todo:  add validation when entering a cloze card so that if the cloze text isn't found in the question, it throws an error.
+//todo:  add some validation for other fields
+//todo:  add some color
+//todo:  add some stuff to the readme file.
+//todo:  look up scope safe constructors
+//todo:  final bug test
+
 //require my card files, and inquirer
 var Cloze = require("./Cloze.js");
 var BasicCard = require("./BasicCard.js");
 var inquirer = require("inquirer");
 
-//Initialize a new Cloze object so we can add questions to the array, I added 10.
+/*Initialize a new Cloze object so we can add questions to the array, 
+I added 10, which is probably more than I needed to add in hindsight.*/
 var newCloze = new Cloze();
 newCloze.addNewCard("A player is considered offside when he is nearer to his opponents goal line than both the ball and the second to last opponent.", "offside");
 newCloze.addNewCard("Players with a Generation Adidas contract don't count against the salary cap in MLS.", "Generation Adidas");
@@ -28,8 +36,9 @@ newBasicCard.addNewCard("Whih player has played the most minutes in MLS history 
 mainMenu();
 
 
-//a Main Menu function uses inquirer to ask the user if they would like to either do basic questions, cloze questions, or add their own cards; then run the function for that choice
-//todo: add validation
+/* Main Menu function uses inquirer to ask the user if they would like to
+either do basic questions, cloze questions, add their own cards or exit;
+then run the function for that choice*/
 function mainMenu() {
     inquirer.prompt([{
         type: "list",
@@ -37,6 +46,7 @@ function mainMenu() {
         choices: ["1. Play Game Using Cloze Cards", "2. Play Game Using Basic Cards", "3. Add My Own Cloze Cards", "4. Add My Own Basic Cards", "5. EXIT GAME"],
         name: "choice"
     }]).then(function(data) {
+
         //switch statement, run function based on their choice:
         switch (data.choice) {
             case '1. Play Game Using Cloze Cards':
@@ -49,24 +59,24 @@ function mainMenu() {
                 createClozeCards();
                 break;
             case "4. Add My Own Basic Cards":
-            	createBasicCards();
-            	break;
+                createBasicCards();
+                break;
             case "5. EXIT GAME":
                 break;
-            //default should never happen in theory because the selections are limited
+            //default case should never happen in theory because the selections are limited
             default:
                 throw "ERROR!";
         }
     });
 }
 
-//Create a function to ask the Cloze Questions in the clozeCardsArr.
-//pass in a count variable that should be zero to start from the beginning
+/*  Create a function to ask the Cloze Questions that are in the clozeCardsArr[].
+pass in a count variable that should be zero to start from the beginning of the array*/
 function askQuestions(count) {
     if (count < newCloze.clozeCardsArr.length) {
         inquirer.prompt([{
             type: "input",
-            message: "___Question"+(count+1)+"___:\n" + newCloze.clozeCardsArr[count].returnPartialText()+"\nAnswer: ",
+            message: "___Question" + (count + 1) + "___:\n" + newCloze.clozeCardsArr[count].returnPartialText() + "\nAnswer: ",
             name: "userInput"
         }]).then(function(data) {
 
@@ -78,10 +88,12 @@ function askQuestions(count) {
                 newCloze.incorrectAnswers++;
             }
 
-            //increment the count and ask again IF there are any questions remaining
+            //increment the question count and ask again IF there are any questions remaining
             count++;
             askQuestions(count);
         });
+
+    //when all questions have been asked, end the game
     } else {
         console.log("GAME OVER!");
         console.log("YOU ANSWERED " + newCloze.correctAnswers + "/" + newCloze.clozeCardsArr.length + " QUESTIONS CORRECTLY!");
@@ -90,7 +102,7 @@ function askQuestions(count) {
             console.log("THAT'S A PERFECT SCORE!");
         }
 
-        //Reset the scores, reset the count
+        //Reset the  scores, reset the count
         //Ask player to play again or return to menu
         newCloze.incorrectAnswers = 0;
         newCloze.correctAnswers = 0;
@@ -101,25 +113,22 @@ function askQuestions(count) {
             choices: ["YES", "NO"],
             name: "choice"
         }]).then(function(data) {
-
             if (data.choice === "YES") {
                 askQuestions(count);
             } else {
                 mainMenu();
             }
         });
-
     }
 }
 
-
-//Create an askBasicQuestions function, basically the same as above
-//but using basic questions instead of Cloze questions.
+/*Create an askBasicQuestions function, basically the same as above
+but using basic questions instead of Cloze questions.*/
 function askBasicQuestions(count) {
     if (count < newBasicCard.basicCardsArr.length) {
         inquirer.prompt([{
             type: "input",
-            message: "___Question"+(count+1)+"___:\n" + newBasicCard.basicCardsArr[count].front+"\nAnswer: ",
+            message: "___Question" + (count + 1) + "___:\n" + newBasicCard.basicCardsArr[count].front + "\nAnswer: ",
             name: "userInput"
         }]).then(function(data) {
             //check the answer and increment scores based on
@@ -163,64 +172,56 @@ function askBasicQuestions(count) {
 }
 
 //Create createClozeCards function so the user can add their own cards
-function createClozeCards(){
-	        inquirer.prompt([
-	        {
-            type: "input",
-            message: "Please enter the question that you would like to add to the Flash Card:\n",
-            name: "userQuestion"
-        	},
-        	{
-        	type: "input",
-        	message: "Please enter the Cloze text (answer) to your question:\n",
-        	name: "userAnswer"
-        	},
-        	{
-        	type: "list",
-        	message: "Would you like to add another Flash Card?",
-        	choices: ["YES", "NO"],
-        	name: "choice"
-        	}
-        	]).then(function(data) {
-        	console.log(data.userQuestion);
-        	console.log(data.userAnswer);
-        	newCloze.addNewCard(data.userQuestion, data.userAnswer);
+function createClozeCards() {
+    inquirer.prompt([{
+        type: "input",
+        message: "Please enter the question that you would like to add to the Flash Card:\n",
+        name: "userQuestion"
+    }, {
+        type: "input",
+        message: "Please enter the Cloze text (answer) to your question:\n",
+        name: "userAnswer"
+    }, {
+        type: "list",
+        message: "Would you like to add another Flash Card?",
+        choices: ["YES", "NO"],
+        name: "choice"
+    }]).then(function(data) {
+        console.log(data.userQuestion);
+        console.log(data.userAnswer);
+        newCloze.addNewCard(data.userQuestion, data.userAnswer);
 
-        	            if (data.choice === "YES") {
-                createClozeCards();
-            } else {
-                mainMenu();
-            }
+        if (data.choice === "YES") {
+            createClozeCards();
+        } else {
+            mainMenu();
+        }
 
-        });
+    });
 }
 
 //Create createBasicCards function so the user can add their own cards
-function createBasicCards(){
-	        inquirer.prompt([
-	        {
-            type: "input",
-            message: "Please enter the question that you would like to add to the front of the Flash Card:\n",
-            name: "userQuestion"
-        	},
-        	{
-        	type: "input",
-        	message: "Please enter the answer to your question:\n",
-        	name: "userAnswer"
-        	},
-        	{
-        	type: "list",
-        	message: "Would you like to add another Flash Card?",
-        	choices: ["YES", "NO"],
-        	name: "choice"
-        	}
-        	]).then(function(data) {
-        	newBasicCard.addNewCard(data.userQuestion, data.userAnswer);
-        	            if (data.choice === "YES") {
-                createBasicCards();
-            } else {
-                mainMenu();
-            }
+function createBasicCards() {
+    inquirer.prompt([{
+        type: "input",
+        message: "Please enter the question that you would like to add to the front of the Flash Card:\n",
+        name: "userQuestion"
+    }, {
+        type: "input",
+        message: "Please enter the answer to your question:\n",
+        name: "userAnswer"
+    }, {
+        type: "list",
+        message: "Would you like to add another Flash Card?",
+        choices: ["YES", "NO"],
+        name: "choice"
+    }]).then(function(data) {
+        newBasicCard.addNewCard(data.userQuestion, data.userAnswer);
+        if (data.choice === "YES") {
+            createBasicCards();
+        } else {
+            mainMenu();
+        }
 
-        });
+    });
 }
